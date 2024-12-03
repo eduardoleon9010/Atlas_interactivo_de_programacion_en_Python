6ea9ef7b7d070b2675e4cd97c9a480670271358e
Ing. Eduardo León <66545075+eduardoleon9010@users.noreply.github.com>
2024-12-03 14:17:11 -0500

    Delete assets directory

--- assets/main.js
+++
@@ -1,4 +0,0 @@
-// Ejemplo de script para interactividad (puedes agregar más funcionalidades)
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("¡Bienvenido al Atlas Interactivo de Programación en Python!");
-});
