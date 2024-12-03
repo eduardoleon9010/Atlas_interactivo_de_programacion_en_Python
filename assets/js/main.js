046512b4e735832486a15bb4d801c0eb1a2f5463
Ing. Eduardo León <66545075+eduardoleon9010@users.noreply.github.com>
2024-12-03 16:35:41 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
+  // Menú desplegable
   const menuItems = document.querySelectorAll('nav ul li');
 
   menuItems.forEach(item => {
     item.addEventListener('click', function(event) {
       const submenu = item.querySelector('.submenu');
-      // Alterna la visibilidad del submenú
-      submenu.style.display = (submenu.style.display === 'block') ? 'none' : '
       
-      // Si el submenú está visible, lo ocultamos; si no lo está, lo mostramos
+      // Alterna la visibilidad del submenú
       if (submenu.style.display === 'block') {
         submenu.style.display = 'none';
       } else {
         submenu.style.display = 'block';
       }
-      
-      // Evitamos que el clic se propague al resto del documento
+
+      // Evita que el clic se propague al resto del documento
       event.stopPropagation();
     });
   });
@@ -25,21 +24,14 @@ document.addEventListener('DOMContentLoaded', function() {
       submenu.style.display = 'none';
     });
   });
-});
 
-
-
-// Esperar a que el contenido de la página se haya cargado completamente
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("¡Bienvenido al Atlas Interactivo de Programación en Python!");
-
-  // Funcionalidad 1: Mensaje de bienvenida interactivo
+  // Mensaje de bienvenida interactivo
   const bienvenida = document.querySelector(".bienvenida");
   if (bienvenida) {
     bienvenida.innerHTML = "¡Explora el Atlas Interactivo para aprender Python!";
   }
 
-  // Funcionalidad 2: Cambiar entre modo claro y oscuro
+  // Cambiar entre modo claro y oscuro
   const themeToggle = document.querySelector("#theme-toggle");
   themeToggle.addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
@@ -53,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function() {
     document.body.classList.add("dark-mode");
   }
 
-  // Funcionalidad 3: Efecto de desplazamiento suave para enlaces
+  // Efecto de desplazamiento suave para enlaces
   const enlaces = document.querySelectorAll('a[href^="#"]');
   enlaces.forEach(function(enlace) {
     enlace.addEventListener("click", function(event) {
@@ -67,4 +59,5 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
   });
+
 });
