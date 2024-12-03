91bdd215ec4bc3594c16aca45eb57ab28e3733ce
Ing. Eduardo León <66545075+eduardoleon9010@users.noreply.github.com>
2024-12-03 13:14:33 -0500

    Add files via upload

---
+++ _layouts/main.js
@@ -0,0 +1,4 @@
+// Ejemplo de script para interactividad (puedes agregar más funcionalidades)
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("¡Bienvenido al Atlas Interactivo de Programación en Python!");
+});
